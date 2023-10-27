5b78f583414c66ea0fb68e1343f7db42dcdf9b69
Addison Hernandez <addison.hernandez@gmail.com>
2023-10-27 17:28:11 -0400

    Fix a bug that added volta to the PATH twice

--- fish/config.fish
+++ fish/config.fish
@@ -14,7 +14,7 @@ if status is-interactive
 
     # Volta
     set -gx VOLTA_HOME "$HOME/.volta"
-    if not contains $VOLTA_HOME $PATH
+    if not contains "$VOLTA_HOME/bin" $PATH
         set -gx PATH "$VOLTA_HOME/bin" $PATH
     end
 end

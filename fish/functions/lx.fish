bd486a0f43628af0d2cc14f09224a23a0fb13c02
Addison Hernandez <addison.hernandez@gmail.com>
2023-10-29 21:31:17 -0400

    Update ls aliases to use eza
    https://github.com/eza-community/eza

--- fish/functions/lx.fish
+++ fish/functions/lx.fish
@@ -1,3 +1,18 @@
-function lx --wraps=ls --description 'List almost all contents of directory, including hidden files, in long format, with classification indicators and human-readable sizes'
-    ls -lhAXF --group-directories-first $argv
+#function lx --wraps=ls --description 'List almost all contents of directory, including hidden files, in long format, with classification indicators and human-readable sizes'
+#    ls -lhAXF --group-directories-first $argv
+#    ls --almost-all --classify --human-readable --format=long --sort=extension
+#end
+
+function lx \
+    --wraps=eza \
+    --description 'List almost all contents of a directory, including hidden files, in long format, with classification indicators'
+    eza \
+        --classify \
+        --long \
+        --all \
+        --sort=type \
+        --group-directories-first \
+        --header \
+        --git \
+        $argv
 end

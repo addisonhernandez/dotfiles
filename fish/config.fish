ce3283e63352011f08a2491580c81298354c018b
Addison Hernandez <addison.hernandez@gmail.com>
2023-10-29 21:32:51 -0400

    Use fish_add_path instead of manipulating PATH

--- fish/config.fish
+++ fish/config.fish
@@ -8,13 +8,9 @@ if status is-interactive
 
     # pnpm
     set -gx PNPM_HOME "$HOME/.local/share/pnpm"
-    if not contains $PNPM_HOME $PATH
-        set -gx PATH $PNPM_HOME $PATH
-    end
+    fish_add_path --path --prepend $PNPM_HOME
 
     # Volta
     set -gx VOLTA_HOME "$HOME/.volta"
-    if not contains "$VOLTA_HOME/bin" $PATH
-        set -gx PATH "$VOLTA_HOME/bin" $PATH
-    end
+    fish_add_path --path --prepend "$VOLTA_HOME/bin"
 end

b4c17af4831370fcdee1a134b3ceb8f15664e1e4
Addison Hernandez <addison.hernandez@gmail.com>
2023-12-15 16:28:38 -0500

    Update upfish completion: apt -> dnf

--- fish/completions/upfish.fish
+++ fish/completions/upfish.fish
@@ -4,7 +4,7 @@ set --local upfish_path /home/addison/.config/fish/functions/upfish.fish
 
 complete --short-option h --long-option help --description "Print a help message and exit" upfish
 complete --short-option C --long-option noclean --description "Skip the package cleanup routine" upfish
-complete --short-option y --long-option yes --description "Upgrade apt packages without prompting" upfish
+complete --short-option y --long-option yes --description "Upgrade dnf packages without prompting" upfish
 complete --short-option c --long-option cargo --description "Update cargo packages" upfish
 complete --short-option F --long-option prompt-flatpak --description "Prompt for flatpak upgrades" upfish
 

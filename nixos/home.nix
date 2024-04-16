f2bd5c21efec0f9239a1ccd58f7ad766214e40e5
Addison Hernandez <addison.hernandez@gmail.com>
2024-04-16 14:17:50 -0400

    Add chromium browser for dev work

--- nixos/home.nix
+++ nixos/home.nix
@@ -160,6 +160,17 @@
         };
       };
     };
+
+    chromium = {
+      enable = true;
+      extensions = [
+        { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
+        { id = "nngceckbapebfimnlniiiahkandclblb"; } # bitwarden
+        { id = "clngdbkpkpeebahjckkjfobafhncgmne"; } # stylus
+        { id = "fmkadmapgofadopljbjfkapdkoienihi"; } # react dev tools
+        { id = "jinjaccalgkegednnccohejagnlnfdag"; } # violentmonkey
+      ];
+    };
   };
 
   # Let Home Manager install and manage itself.

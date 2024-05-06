bdb1298c2dde956e4af481299eb26ccadc8adcfb
Addison Hernandez <addison.hernandez@gmail.com>
2024-05-06 15:18:46 -0400

    Migrate config.fish -> conf.d/

--- fish/conf.d/11_interactive.fish
+++ fish/conf.d/11_interactive.fish
@@ -3,8 +3,8 @@ if status is-interactive
     abbr --add dotdot --regex '^\.\.+$' --function multicd
 
     # Set vim as default editor
-    set -gx EDITOR $(command -v vim)
-    set -gx VISUAL $(command -v vim)
+    set -gx EDITOR $(command -v nvim)
+    set -gx VISUAL $(command -v nvim)
 
     # Add fzf key bindings
     command -q fzf
